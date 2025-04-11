5aaa473dcee8cea7447342ea1d75890873d12516
M.R Avaz <avazaghaei@yahoo.com>
2025-04-11 12:57:29 +0330

    Add Increment and Decrement command classes

--- CommandProcessor/Classes/ClassDecimalProcess.cs
+++ CommandProcessor/Classes/ClassDecimalProcess.cs
@@ -8,5 +8,49 @@ namespace CommandProcessor.Classes
 {
     internal class ClassDecimalProcess
     {
+        public static int sharedNumber { get; set; }
+
+        public ClassDecimalProcess(int initial)
+        {
+            sharedNumber = initial;
+        }
+    }
+
+    public interface IDecimalProcess
+    {
+        void exec();
+        void undo();
+    }
+
+    public class classIncrement
+    {
+        private static int number;
+        public static int funcUserID
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public void exec() 
+        {
+            number++;
+        }
+        public void undo() 
+        {
+            number--;
+        }
+
+    }
+
+    public class classDecrement()
+    {
+        public void exec(int value)
+        {
+            value++;
+        }
+        public void undo(int value)
+        {
+            value--;
+        }
     }
 }
